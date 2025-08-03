c53ce53923bb8b2d658f081ba542900ef39902b0
GitHub Action <action@github.com>
2025-08-03 23:39:35 +0000

    Bump version: 0.1.0 → 0.1.1

--- src/owasp_schema/__init__.py
+++ src/owasp_schema/__init__.py
@@ -7,7 +7,7 @@ import importlib.resources
 import json
 from typing import Any
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "Arkadii Yakovets <arkadii.yakovets@owasp.org>"
 __license__ = "MIT"
 
